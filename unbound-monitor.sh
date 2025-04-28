0bf0d0bfe9ff4daf4c044dc191e5c12c12216770
user65434680 <s2kava01@students.osao.fi>
2025-04-28 12:30:29 +0300

    more time for dig

--- unbound-monitor.sh
+++ unbound-monitor.sh
@@ -29,8 +29,16 @@ while true; do
     fi
 
     for domain in $domains; do
-        resolved_ip=$(dig +short $domain)
+        echo "Resolving domain: $domain"
+        resolved_ip=$(dig +short +time=10 $domain)
+        
+        if [ -z "$resolved_ip" ]; then
+            echo "Error: No IPs resolved for $domain."
+            continue
+        fi
+
         for ip in $resolved_ip; do
+            echo "Checking if iptables rule exists for $ip"
             if ! iptables -C OUTPUT -d $ip -j ACCEPT 2>/dev/null; then
                 echo "Allowing outgoing traffic to $domain ($ip)"
                 iptables -A OUTPUT -d $ip -j ACCEPT
