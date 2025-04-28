21950035fff54ace41c0d401d9d27bc6fe2e6431
user65434680 <s2kava01@students.osao.fi>
2025-04-28 13:54:16 +0300

    more testing

--- install.sh
+++ install.sh
@@ -8,13 +8,15 @@ sudo systemctl enable unbound
 sudo apt install openssh-server -y
 sudo systemctl enable ssh
 sudo apt install -y inotify-tools iptables ipset iptables-persistent
-sudo apt install jq
+sudo apt install jq -y
+sudo apt install dnsutils -y
 
 chmod +x echo_ip_range.sh
 chmod +x network.sh
 chmod +x add_to_sudoers.sh
 chmod +x IP_tables_install.sh
-chmod +x unbound-monitor.sh
+chmod +x resolve_domains.sh
+chmod +x block_unauthorized_ips.sh
 sudo bash echo_ip_range.sh
 sudo bash network.sh
 sudo bash add_to_sudoers.sh
